a8292845547bb3fe1d4a317bb6fdb82ff3db41bf
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-09-17 21:47:15 -0400

    Added back to top button, styled cards, added websites to card btns, styled why-stevens box, and unfixed navbar. Want to work on hr, scroll bar, and descriptions

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ const majors = [
     {
         name: 'Mechanical Engineering',
         description: 'hello',
-        img: 'imgs/STIT-mechanical-engineering.jpg'
+        img: 'imgs/STIT-mechanical-engineering.jpg',
+        link: 'https://www.stevens.edu/program/mechanical-engineering-masters'
     },
     {
         name: 'Computer Science',
         description: 'helloo',
-        img: 'imgs/STIT-computer-science.png'
+        img: 'imgs/STIT-computer-science.png',
+        link: 'https://www.stevens.edu/program/computer-science-bachelor-degree'
     },
     {
         name: 'Cyber Security',
         description: 'hellooo',
-        img: 'imgs/STIT-cyber-security.png'
+        img: 'imgs/STIT-cyber-security.png',
+        link: 'https://www.stevens.edu/program/cybersecurity-bachelor-degree'
     }
 ]
 
@@ -27,12 +30,11 @@ function generateMajorCards() {
 
         card.innerHTML = `
             <div class = "card p-c h-100" id = "card">
-                <div class = "card-header text-center">${major.name}</div>
-                <div class = "card-body">
-                    <div class = "text-center">
-                        <img class = "major-img" src = "${major.img}">
-                        <p class = "p-3">${major.description}</p>
-                    </div>
+                <img src = "${major.img}" class = "card-img-top">
+                <div class = "card-body text-center">
+                    <h5 class="card-title">${major.name}</h5>
+                    <p class="card-text">${major.description}</p>
+                    <a href="${major.link}" target="_blank" class="btn more-info-btn">More Information</a>
                 </div>
             </div>
         `;
@@ -40,7 +42,40 @@ function generateMajorCards() {
     })
 
     function refresh() {
-    location.reload()
- }}
+        location.reload()
+    }
+}
 
- window.onload = generateMajorCards();
+window.onload = generateMajorCards();
+
+{/* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> */}
+
+
+//  Back to top button
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
